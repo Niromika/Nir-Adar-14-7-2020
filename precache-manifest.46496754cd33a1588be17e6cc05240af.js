0aa77e2ad88c0f2e1a68d6b1705abbbf87ec2a76
Nir Adar <niromika88@gmail.com>
2020-07-19 12:10:20 +0300

    Updates

---
+++ precache-manifest.46496754cd33a1588be17e6cc05240af.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1905688eef5587fee1794b4e3e80cc1b",
+    "url": "/Nir-Adar-14-7-2020/index.html"
+  },
+  {
+    "revision": "4814df2790b1735a20ea",
+    "url": "/Nir-Adar-14-7-2020/static/css/main.9f26d9fc.chunk.css"
+  },
+  {
+    "revision": "50e4f36054d608984c3f",
+    "url": "/Nir-Adar-14-7-2020/static/js/2.d529875b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nir-Adar-14-7-2020/static/js/2.d529875b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4814df2790b1735a20ea",
+    "url": "/Nir-Adar-14-7-2020/static/js/main.86790c97.chunk.js"
+  },
+  {
+    "revision": "00ccc77dc013fcbe079e",
+    "url": "/Nir-Adar-14-7-2020/static/js/runtime-main.97324d31.js"
+  },
+  {
+    "revision": "448a52622f6ff192bc0f47d68fc704ff",
+    "url": "/Nir-Adar-14-7-2020/static/media/1.448a5262.png"
+  },
+  {
+    "revision": "bbc34bb953d2bd119467846a1fc69b14",
+    "url": "/Nir-Adar-14-7-2020/static/media/11.bbc34bb9.png"
+  },
+  {
+    "revision": "0c6e601635f314bb003eb7c1f2ccb994",
+    "url": "/Nir-Adar-14-7-2020/static/media/12.0c6e6016.png"
+  },
+  {
+    "revision": "d45888c4303377b1bd51263075b276b2",
+    "url": "/Nir-Adar-14-7-2020/static/media/13.d45888c4.png"
+  },
+  {
+    "revision": "56ad48ec770b64e576b36bd53d856068",
+    "url": "/Nir-Adar-14-7-2020/static/media/14.56ad48ec.png"
+  },
+  {
+    "revision": "496b3b200dd0f8a04c29cc41785ee36a",
+    "url": "/Nir-Adar-14-7-2020/static/media/15.496b3b20.png"
+  },
+  {
+    "revision": "a2693d85daa9caf17ec4bc4c8ba00266",
+    "url": "/Nir-Adar-14-7-2020/static/media/16.a2693d85.png"
+  },
+  {
+    "revision": "be62975f35990883e0d70c079940df0c",
+    "url": "/Nir-Adar-14-7-2020/static/media/17.be62975f.png"
+  },
+  {
+    "revision": "2477303a8baadab67a5cfdfbb2a2280a",
+    "url": "/Nir-Adar-14-7-2020/static/media/18.2477303a.png"
+  },
+  {
+    "revision": "f16170da6aab57fc74fa5deb7a09fa8f",
+    "url": "/Nir-Adar-14-7-2020/static/media/19.f16170da.png"
+  },
+  {
+    "revision": "b3cc54bc9e145158523fe09ed514210c",
+    "url": "/Nir-Adar-14-7-2020/static/media/2.b3cc54bc.png"
+  },
+  {
+    "revision": "6db1f1c03b31105a0b48e66eaf9247d1",
+    "url": "/Nir-Adar-14-7-2020/static/media/20.6db1f1c0.png"
+  },
+  {
+    "revision": "573d6d060418c606409f8d5147455fe7",
+    "url": "/Nir-Adar-14-7-2020/static/media/21.573d6d06.png"
+  },
+  {
+    "revision": "284e6e4a8ed034b28deb0ee8584fbbd7",
+    "url": "/Nir-Adar-14-7-2020/static/media/22.284e6e4a.png"
+  },
+  {
+    "revision": "8d654e298db29739599e0a6349c746b1",
+    "url": "/Nir-Adar-14-7-2020/static/media/23.8d654e29.png"
+  },
+  {
+    "revision": "3d1a013e6dcda328b95606a938786ade",
+    "url": "/Nir-Adar-14-7-2020/static/media/25.3d1a013e.png"
+  },
+  {
+    "revision": "ea2c9a468e41d70bb32f10045e7034fc",
+    "url": "/Nir-Adar-14-7-2020/static/media/26.ea2c9a46.png"
+  },
+  {
+    "revision": "890944aad0cf4a36ed96012c98b39e4b",
+    "url": "/Nir-Adar-14-7-2020/static/media/29.890944aa.png"
+  },
+  {
+    "revision": "d4e57486023edb4377ae82c03f03dd7d",
+    "url": "/Nir-Adar-14-7-2020/static/media/3.d4e57486.png"
+  },
+  {
+    "revision": "349dda09dab68ed636acac03eb4941a4",
+    "url": "/Nir-Adar-14-7-2020/static/media/33.349dda09.png"
+  },
+  {
+    "revision": "938f87694f8a495e00536148e66c76ef",
+    "url": "/Nir-Adar-14-7-2020/static/media/34.938f8769.png"
+  },
+  {
+    "revision": "61da31ff4bdaf78b485391acdce1efa5",
+    "url": "/Nir-Adar-14-7-2020/static/media/35.61da31ff.png"
+  },
+  {
+    "revision": "88f99eccb18ebbddf2122b1238019a8b",
+    "url": "/Nir-Adar-14-7-2020/static/media/36.88f99ecc.png"
+  },
+  {
+    "revision": "9a6d6c7597c05b3b15b976374ebefa2c",
+    "url": "/Nir-Adar-14-7-2020/static/media/37.9a6d6c75.png"
+  },
+  {
+    "revision": "efc65f0d4616bb06756011d3ae170283",
+    "url": "/Nir-Adar-14-7-2020/static/media/38.efc65f0d.png"
+  },
+  {
+    "revision": "a927037eb6253926716dd1489301b66e",
+    "url": "/Nir-Adar-14-7-2020/static/media/39.a927037e.png"
+  },
+  {
+    "revision": "ace19d752e645dc477730f23bd6e69bb",
+    "url": "/Nir-Adar-14-7-2020/static/media/4.ace19d75.png"
+  },
+  {
+    "revision": "c0ff4c36a9155a0ad78760125690f3a6",
+    "url": "/Nir-Adar-14-7-2020/static/media/40.c0ff4c36.png"
+  },
+  {
+    "revision": "cab841ac5b9dfd9ad378edf3353bd3d0",
+    "url": "/Nir-Adar-14-7-2020/static/media/41.cab841ac.png"
+  },
+  {
+    "revision": "31d8b48e1e4f5efce7ca5455e33f4d3d",
+    "url": "/Nir-Adar-14-7-2020/static/media/42.31d8b48e.png"
+  },
+  {
+    "revision": "6f902cc458be56de06334e2a7e3e3eb2",
+    "url": "/Nir-Adar-14-7-2020/static/media/43.6f902cc4.png"
+  },
+  {
+    "revision": "a304203c0f4758f62461c7875835c2c1",
+    "url": "/Nir-Adar-14-7-2020/static/media/44.a304203c.png"
+  },
+  {
+    "revision": "3ff7cd1b917e35424731fd9c33aa18ad",
+    "url": "/Nir-Adar-14-7-2020/static/media/5.3ff7cd1b.png"
+  },
+  {
+    "revision": "39ec473924f0da17b426d467a5b85248",
+    "url": "/Nir-Adar-14-7-2020/static/media/6.39ec4739.png"
+  },
+  {
+    "revision": "5cb0a006a6f7e54ceb14485a5ad7b3e8",
+    "url": "/Nir-Adar-14-7-2020/static/media/7.5cb0a006.png"
+  },
+  {
+    "revision": "e2bd4b3eee6c33135e468a2595e8f231",
+    "url": "/Nir-Adar-14-7-2020/static/media/8.e2bd4b3e.png"
+  }
+]);
